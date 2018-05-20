51aa5533e2f02b4291875715612db481a61bac0b
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-20 06:41:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var katzDeli = [];
 
 function takeANumber(katzDeli, name)
 {
-  katzDeli.length + 1
+  katzDeli.push(name)
   return ("Welcome" + name + ". You are number " + (katzDeli.length) + " in line.")
 }
 
